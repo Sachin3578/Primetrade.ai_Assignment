2ca3250feb1bf470d502d333191a1126f8a4d909
sachin gornar <gornarsachin2004@gmail.com>
2025-10-07 15:06:03 +0530

    cors setup

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "https://primetrade-ai-assignment-se3d.onrender.com",
+  baseURL: "https://primetrade-ai-assignment-se3d.onrender.com/api",
   withCredentials: true,
 });
 
