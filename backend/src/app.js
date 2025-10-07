e3975050c351f135e5f3b10d6517ab0f32005561
sachin gornar <gornarsachin2004@gmail.com>
2025-10-07 14:17:30 +0530

    client uri added

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,8 @@ const adminRoutes = require("./routes/adminRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/admin", adminRoutes);
