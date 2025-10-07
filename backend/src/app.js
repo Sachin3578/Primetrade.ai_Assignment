b0d4537d9ba33c6dd4f06364eef655fa4effdb32
sachin gornar <gornarsachin2004@gmail.com>
2025-10-07 14:44:50 +0530

    cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
 const authRoutes = require("./routes/auth");
 const taskRoutes = require("./routes/taskRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 
+
 app.use(express.json());
-//app.use(cors());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, 
+    credentials: true,              
+  })
+);
+
+// PI Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/admin", adminRoutes);
 
-
+// est Route
 app.get("/", (req, res) => {
-    res.json({message: "API is running"});
+  res.json({ message: "API is running ✅" });
 });
+
 
 module.exports = app;
